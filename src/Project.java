e3fec7bff491c789211dead2958eb2f2069e025c
bhavin0810 <bhavin0810@gmail.com>
2015-01-22 15:47:51 -0500

    Create basic Project Class

---
+++ src/Project.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bhavin Patel
+ * @Description: Demo Files for COMP1001 Lesson 3
+ * @version: 1.0 Create basic Project Class
+ *
+ *
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
